3f87a03f5072cd3e13ec6219541d085d80f9d1db
asrioth <liamswords@gmail.com>
2024-12-16 11:34:55 +1300

    Added book report functionality to main

--- main.py
+++ main.py
@@ -11,10 +11,28 @@ def bookCharachterCount(book_contents):
         charachter_count[charachter.lower()] += 1
     return charachter_count
 
+def printCharachterCounts(charachter_count):
+    for character in charachter_count:
+        charachter_count_text = f"The '{character}' appeared {charachter_count[character]} times"
+        print(charachter_count_text)
+
+def printBookReport(book_contents, book_path):
+    header_text = f"*** Counting report for {book_path} ***"
+    footer_text = "*** Counting report end ***"
+    word_count = bookWordCount(book_contents)
+    word_count_text = f"{word_count} words found in {book_path}"
+    charachter_count = bookCharachterCount(book_contents)
+    print(header_text)
+    print(word_count_text)
+    print() #spacer
+    printCharachterCounts(charachter_count)
+    print(footer_text)
+
 def main():
-    with open("./books/frankenstein.txt") as book:
+    frankenstein_path ="books/frankenstein.txt"
+    with open(frankenstein_path) as book:
         book_contents = book.read()
-        print(bookWordCount(book_contents))
-        print(bookCharachterCount(book_contents))
+        printBookReport(book_contents, frankenstein_path)
+        
 
 main()
